f33f2bd76a85778bd4c412ebae965b3ba5c77ea5
tsundere-cafe <kaypav@gmail.com>
2014-05-20 20:34:36 -0600

    add to github

---
+++ Tap/Assets/Scripts/Tap.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+
+namespace Tap {
+    public static class Tap
+    {
+        public static T Tap<T>(this T tap, Action<T> act)
+        {
+            act(tap);
+            return tap;
+        }
+
+        public static T Log<T>(this T tap)
+        {
+            Debug.Log(tap);
+            return tap;
+        }
+
+        public static T Log<T, S>(this T tap, Func<T, S> func)
+        {
+            Debug.Log(func(tap));
+            return tap;
+        }
+
+        public static S AndAnd<T, S>(this T tap, Func<T, S> func)
+        {
+            if (tap == null || tap is UnityEngine.Object && !(tap as UnityEngine.Object))
+            {
+                return default(S);
+            }
+
+            return func(tap);
+        }
+    }
+}
---
+++ Tap/Assets/Tests/TapTest.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tap;
+using Shouldly;
+
+public class TapTest : TestBehaviour {
+    string it;
+    string passedIn;
+    string ret;
+    bool toUpperCalled = false;
+
+    public override void Spec()
+    {
+        Scenario("uses Tap");
+
+        Given("it is the string 'hi'")
+            .When("it is tapped")
+            .Then("the string 'hi' should be passed into the tap")
+            .Because("tap passes the instance it is called on into the function it is given");
+
+        Given("it is the string 'hi'")
+            .When("it is tapped")
+            .Then("the tap should return the string 'hi'")
+            .Because("tap returns the instance it was called on");
+
+        Given("it is the string 'hi'")
+            .When("ToUpper is passed to AndAnd")
+            .Then("AndAnd should return 'HI'")
+            .Because("AndAnd passes the instance it is called on into the function it is given and returns the result");
+
+        Given("it is a null string")
+            .When("ToUpper is passed to AndAnd")
+            .Then("ToUpper should not be called")
+            .And("AndAnd should return null")
+            .Because("AndAnd does not call the function if it is called on null");
+    }
+
+    public void ItIsTheString__(string str)
+    {
+        it = str;
+    }
+
+    public void ItIsANullString()
+    {
+        it = null;
+    }
+
+    public void ItIsTapped()
+    {
+        ret = it.Tap(_ => passedIn = _);
+    }
+
+    public void ToUpperIsPassedToAndAnd()
+    {
+        ret = it.AndAnd(_ => {
+            toUpperCalled = true;
+            return _.ToUpper();
+        });
+    }
+
+    public void TheString__ShouldBePassedIntoTheTap(string expected)
+    {
+        passedIn.ShouldBe(expected);
+    }
+
+    public void TheTapShouldReturnTheString__(string expected)
+    {
+        ret.ShouldBe(expected);
+    }
+
+    public void AndAndShouldReturn__(string expected)
+    {
+        ret.ShouldBe(expected);
+    }
+
+    public void ToUpperShouldNotBeCalled()
+    {
+        toUpperCalled.ShouldBe(false);
+    }
+
+    public void AndAndShouldReturnNull()
+    {
+        ret.ShouldBe(null);
+    }
+}
